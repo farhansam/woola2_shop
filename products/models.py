36fa0e3a30a19bf51afcae37337b80de50ddf2d5
farhansam <farhan_samsol@hotmail.com>
2021-05-03 13:51:34 +0000

    created model product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+    description = models.TextField(blank=False)
+    price = models.IntegerField(blank=False)
+    stock = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return self.name
