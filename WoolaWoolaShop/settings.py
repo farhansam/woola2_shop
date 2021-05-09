9e393b26613d9c1ba71876bcb6e87922d0f77ba1
farhansam <farhan_samsol@hotmail.com>
2021-05-09 07:40:44 +0000

    installed crispy forms successful

--- WoolaWoolaShop/settings.py
+++ WoolaWoolaShop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
     'products',
 ]
 
@@ -147,6 +148,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
