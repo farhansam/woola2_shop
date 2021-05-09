ea10ca9daa8e401779f46a45528ea085ea46bc51
farhansam <farhan_samsol@hotmail.com>
2021-05-09 04:56:49 +0000

    created model collection

--- products/models.py
+++ products/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.TextField(blank=False)
     price = models.IntegerField(blank=False)
     stock = models.IntegerField(blank=False)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
