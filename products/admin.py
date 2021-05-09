d8c8958901b974abd3ba5c3b216ddd31be45f8fa
farhansam <farhan_samsol@hotmail.com>
2021-05-09 05:08:33 +0000

    created model tag

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Collection
+from .models import Product, Collection, Tag
 
 admin.site.register(Product)
 admin.site.register(Collection)
+admin.site.register(Tag)
--- products/models.py
+++ products/models.py
@@ -7,12 +7,19 @@ class Collection(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.TextField(blank=False)
     price = models.IntegerField(blank=False)
     stock = models.IntegerField(blank=False)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
