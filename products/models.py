664da91f622edbc01a5e235b7525f29c65beb6fa
farhansam <farhan_samsol@hotmail.com>
2021-05-09 09:21:07 +0000

    installed uploadcare, image display successful

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 class Collection(models.Model):
@@ -20,6 +21,7 @@ class Product(models.Model):
     stock = models.IntegerField(blank=False)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+    image = ImageField(blank=True, manual_crop="")
 
     def __str__(self):
         return self.name
