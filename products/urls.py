1d68423acde6de31094b854d1e0b98e8625e8489
farhansam <farhan_samsol@hotmail.com>
2021-05-10 11:06:46 +0000

    create product form successful

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import products.views
 
 urlpatterns = [
-    path('', products.views.index)
+    path('', products.views.index),
+    path('create/', products.views.create_product)
 ]
