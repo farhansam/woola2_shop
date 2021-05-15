6960a3fecfd48a9f3d96ade87a232b08c287e204
farhansam <farhan_samsol@hotmail.com>
2021-05-15 10:33:57 +0000

    update earring successful

--- earrings/urls.py
+++ earrings/urls.py
@@ -3,5 +3,6 @@ import earrings.views
 
 urlpatterns = [
     path('', earrings.views.index),
-    path('create/', earrings.views.create_earring)
+    path('create/', earrings.views.create_earring),
+    path('update/<earring_id>', earrings.views.update_earring, name='update_earring_route')
 ]
--- earrings/views.py
+++ earrings/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
 from .models import Earring
 from .forms import EarringForm
 
 # Create your views here.
+
+
 def index(request):
     all_earrings = Earring.objects.all()
     return render(request, "earrings/index-template.html", {
-        'all_earrings':all_earrings
+        'all_earrings': all_earrings
     })
 
+
 def create_earring(request):
-    create_form = EarringForm()
-    return render(request, 'earrings/create-template.html', {
-        'form':create_form
-    })
+    if request.method == 'POST':
+        create_form = EarringForm(request.POST)
+
+        if create_form.is_valid():
+            create_form.save()
+            return redirect(reverse(index))
+        else:
+            return render(request, 'earrings/create-template.html', {
+                'form': create_form
+            })
+    else:
+        create_form = EarringForm()
+        return render(request, 'earrings/create-template.html', {
+            'form': create_form
+        })
+
+
+def update_earring(request, earring_id):
+    earring_being_updated = get_object_or_404(Earring, pk=earring_id)
+
+    if request.method == 'POST':
+        update_form = EarringForm(request.POST, instance=earring_being_updated)
+
+        if update_form.is_valid():
+            update_form.save()
+            return redirect(reverse(index))
+        else:
+            return render(request, 'earrings/update-template.html', {
+                'form': update_form
+            })
+    else:
+        update_form = EarringForm(instance=earring_being_updated)
+        return render(request, 'earrings/update-template.html', {
+            'form': update_form
+        })
