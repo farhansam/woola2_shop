d67a73f2f0d12063a4ade739b1b3622d13949118
farhansam <farhan_samsol@hotmail.com>
2021-05-16 10:20:00 +0000

    changed database

--- WoolaWoolaShop/settings.py
+++ WoolaWoolaShop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,12 +115,14 @@ WSGI_APPLICATION = 'WoolaWoolaShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ["DATABASE_URL"])}
 
 
 # Password validation
