6ac447e46c6f103c41013a8797f3cf3a5c99c9cc
farhansam <farhan_samsol@hotmail.com>
2021-05-22 13:49:21 +0000

    added create route

--- earrings/urls.py
+++ earrings/urls.py
@@ -3,7 +3,7 @@ import earrings.views
 
 urlpatterns = [
     path('', earrings.views.index, name='show_earring_route'),
-    path('create/', earrings.views.create_earring),
+    path('create/', earrings.views.create_earring, name='create_earring_route'),
     path('update/<earring_id>', earrings.views.update_earring, name='update_earring_route'),
     path('delete/<earring_id>', earrings.views.delete_earring, name='delete_earring_route')
 ]
