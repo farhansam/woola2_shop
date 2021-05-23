37ae0b97662c2c1241fc2e9c56c7fef63fc8a5c7
farhansam <farhan_samsol@hotmail.com>
2021-05-23 12:29:20 +0000

    form validation done

--- earrings/models.py
+++ earrings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Collection(models.Model):
@@ -18,8 +19,8 @@ class Tag(models.Model):
 class Earring(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.TextField(blank=False)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
-    stock = models.IntegerField(blank=False)
+    price = models.DecimalField(validators=[MinValueValidator(0.01),MaxValueValidator(100000)], max_digits=10, decimal_places=2, blank=False)
+    stock = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100000)], blank=False)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     tags = models.ManyToManyField('Tag')
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
